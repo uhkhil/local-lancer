9dcd031557a1208478c80b666c8155803c457ccd
Akhil <akhilnareshkumar@gmail.com>
2019-12-13 19:37:08 +0530

    Clean up project card code.

--- src/components/ProjectCard/ProjectCardStyles.js
+++ src/components/ProjectCard/ProjectCardStyles.js
@@ -2,15 +2,38 @@ import {StyleSheet} from 'react-native';
 import {Colors} from '../../theme/Theme';
 
 export const styles = StyleSheet.create({
+  cardContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    margin: 40,
+    marginTop: 20,
+    alignSelf: 'center',
+  },
   card: {
     borderRadius: 10,
   },
   cardItemTop: {borderTopLeftRadius: 10, borderTopRightRadius: 10},
-  cardItemBottom: {borderBottomLeftRadius: 10, borderBottomRightRadius: 10},
+  cardItemBottom: {
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    marginBottom: 10,
+  },
   title: {
     color: Colors.primaryColor,
     fontWeight: '900',
     // fontSize: 33,
     // lineHeight: 38,
+  },
+  domainContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  domain: {
+    margin: 5,
+  },
+  domainMore: {
+    margin: 5,
   },
 });
